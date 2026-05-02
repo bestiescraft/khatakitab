f40555b50a72c42f0ec71b85cffdfdc4fff4e2f5
Besties Craft <akkuyadav1434@gmail.com>
2026-05-02 19:51:04 +0530

    remove base for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  base: '/khatakitab/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
